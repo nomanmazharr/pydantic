2970460b50e0dd43b05ed87a685e27d153de5a51
Noman <nmazhar911@gmail.com>
2025-06-13 07:27:17 +0500

    assigning a field a pydantic model for reusability and and readability

---
+++ nested_models.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+
+class Address(BaseModel):
+    city: str
+    state: str
+    Postcode: int
+
+class Player(BaseModel):
+    name: str
+    age: int
+    address: Address
+
+
+player_address  = {'city': 'Faisalabad', 'state': 'punjab', 'Postcode': 38000}
+
+address = Address(**player_address)
+
+player_biodata = {'name': 'Jon', 'age': 30, 'address': address}
+
+player = Player(**player_biodata)
+
+print(player)
+temp = player.model_dump()
+print(type(temp))
